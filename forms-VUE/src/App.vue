e9c0212e0746493fd85ad5de7904fb3c7b10c1fb
xavergruber <xavergruber@webundsoehne.com>
2022-08-03 10:37:12 +0200

    initial commit

---
+++ forms-VUE/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Forms</h1>
+  <div class="container">
+
+
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+<style scoped>
+.container{
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+p{
+  text-align: right;
+}
+
+
+</style>
